b59b0b9ff6f786a3e7471729c63868a3ead018a7
Ivan <hammer.99.ivan@gmail.com>
2022-05-17 18:04:44 +0700

    fix styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import sliderExample from './assets/images/slider-example.png'
 import sliderExample2 from './assets/images/slider-example2.jpg'
 import sliderExample3 from './assets/images/slider-example3.jpg'
+import sliderExample4 from './assets/images/slider-example4.jpg'
 
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
@@ -21,11 +22,12 @@ function App() {
        cssModule={AwsSliderStyles}
        play={true}
        cancelOnInteraction={false} 
-       interval={6000}
+       interval={9000}
        >
     <div data-src={sliderExample} />
     <div data-src={sliderExample2} />
     <div data-src={sliderExample3} />
+    <div data-src={sliderExample4} />
 
   </AutoplaySlider>
   </div>
