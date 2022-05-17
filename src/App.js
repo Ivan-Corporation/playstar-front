bf160d877215d5511828834a4fa2f11b52fce378
Ivan <hammer.99.ivan@gmail.com>
2022-05-17 15:33:34 +0700

    more examples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './assets/styles/App.css';
-import sliderExample from './assets/images/slider-example.png'
 import AwesomeSlider from 'react-awesome-slider';
 import AwsSliderStyles from './assets/styles/core/styles.scss';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
+
+
+import sliderExample from './assets/images/slider-example.png'
+import sliderExample2 from './assets/images/slider-example2.jpg'
+import sliderExample3 from './assets/images/slider-example3.jpg'
+
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
@@ -19,8 +24,8 @@ function App() {
        interval={6000}
        >
     <div data-src={sliderExample} />
-    <div data-src={sliderExample} />
-    <div data-src={sliderExample} />
+    <div data-src={sliderExample2} />
+    <div data-src={sliderExample3} />
 
   </AutoplaySlider>
   </div>
